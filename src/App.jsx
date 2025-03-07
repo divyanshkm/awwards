e8a69bdeaa2373978b35958c9702e72e071b9b83
Divyansh Mahavar <79765685+divyanshkm@users.noreply.github.com>
2025-03-07 20:47:27 +0530

    Tailwind setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="font-bold text-5xl text-yellow-300  ">
+        Welcome to AWWWARDS
+      </h1>
+    </main>
+  );
+};
+
+export default App;
