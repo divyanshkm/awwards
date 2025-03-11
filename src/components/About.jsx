0508728b3e7b7abe4333ff3d618b0d4da049453b
Divyansh Mahavar <79765685+divyanshkm@users.noreply.github.com>
2025-03-11 18:11:22 +0530

    add About Section

---
+++ src/components/About.jsx
@@ -0,0 +1,58 @@
+import { useGSAP } from "@gsap/react";
+import React from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import AnimatedTitle from "./AnimatedTitle";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const About = () => {
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: 0,
+    });
+  });
+
+  return (
+    <div id="about" className="min-h-screen w-screen">
+      <div className="relative mb-5 mt-36 flex-col flex items-center gap-5">
+        <h2 className="font-general text-sm uppercase md:text-[10px]">
+          Welcome to Zentry
+        </h2>
+        <AnimatedTitle />
+
+        <div className="about-subtext">
+          <p>The games of games begins, Start your adventure with Zentry.</p>
+          <p>
+            Zentry unites players from all around the world in a shared
+            metaverse.
+          </p>
+        </div>
+      </div>
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.webp"
+            alt="background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AnimatedTitle = () => {
+  return (
+    <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+      Dis<b>c</b>over the World's
+      <br /> l<b>a</b>rgest shared metaverse.
+    </div>
+  );
+};
+
+export default AnimatedTitle;
